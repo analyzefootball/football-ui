d3fb8348375c871773b0468de3add10c5f600ab9
Hassan Mushtaq <hmushtaq@gmail.com>
2018-05-12 16:29:34 -0500

    Basic structure

---
+++ src/main/java/football/analyze/Application.java
@@ -0,0 +1,16 @@
+package football.analyze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring boot application starting point.
+ * @author hassan
+ * @since 5/7/18
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String... args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
