9053265e94798f2ae24b84fdb1e8bafa83fafbae
Hassan Mushtaq <hmushtaq@gmail.com>
2018-05-13 21:22:05 -0500

    styling

--- src/main/java/football/analyze/uiapp/ApplicationUI.java
+++ src/main/java/football/analyze/uiapp/ApplicationUI.java
@@ -4,20 +4,19 @@ import com.vaadin.annotations.Theme;
 import com.vaadin.event.LayoutEvents;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.Notification;
-import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.*;
 
-@Theme("valo")
+@Theme("football")
 @SpringUI
 public class ApplicationUI extends UI {
     @Override
     protected void init(VaadinRequest request) {
         VerticalLayout vertical = new VerticalLayout();
         Label label = new Label("Coming soon");
+        label.setStyleName("my-label");
         vertical.setSizeFull();
         vertical.addComponent(label);
+        vertical.setComponentAlignment(label, Alignment.MIDDLE_CENTER);
         vertical.addLayoutClickListener((LayoutEvents.LayoutClickListener) event -> Notification.show("Be Patient",
                 Notification.Type.HUMANIZED_MESSAGE));
         setContent(vertical);
